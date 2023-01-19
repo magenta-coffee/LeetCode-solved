d918aeb73cfaec87c1f39e16609b9658934d3061
magenta-coffee <122341633+magenta-coffee@users.noreply.github.com>
2023-01-18 23:59:37 -0800

    Time: 18 ms (73.69%), Space: 13.6 MB (12.61%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            print(needle)
+            i = haystack.index(needle)
+            return i
+        return -1
